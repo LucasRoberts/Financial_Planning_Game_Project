8171308a1e651c44ee96b1c978b3ec39c0779e28
Lucas Roberts <LucasJRoberts36@gmail.com>
2022-02-26 18:09:13 -0500

    - Init Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Author: Lucas Roberts
+Date: 2/26/22
+Description: This program is going to be the computer version of a game that was created by me and my teammates for our
+Financial Planning Course Project. It will be a single player game whose aim is to teach players about saving for
+retirement and Investing. As such the goal of the game is to reach a certain amount of money by the end of X turns while
+managing expenses such as insurance, healthcare, rent and others.
+"""
